cbc01707dac5365b9135d73dd0d496f3e11b2638
shawroshan764 <shawroshan764@gmail.com>
2023-01-22 11:05:23 +0530

    refresh token completed

--- helpers/jwtHelper.js
+++ helpers/jwtHelper.js
@@ -7,7 +7,7 @@ module.exports = {
       const payload = {};
       const secret = process.env.ACCESS_TOKEN_SECRET;
       const options = {
-        expiresIn: "1h",
+        expiresIn: "30s",
         issuer: "pickerpage.com",
         audience: userId,
       };
@@ -24,15 +24,55 @@ module.exports = {
   verifyAccesstoken: (req, res, next) => {
     if (!req.headers["authorization"]) return next(createError.Unauthorized());
     const authHeader = req.headers["authorization"];
-
-    const bearerToken = authHeader.split(' ');
+    const bearerToken = authHeader.split(" ");
     const token = bearerToken[1];
     JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
       if (err) {
-        return next(createError.Unauthorized());
+        // if(err.name === 'JsonWebTokenError'){
+        //   return next(createError.Unauthorized());
+        // }else{
+        //   return next(createError.Unauthorized(err.message))
+        // }
+        const message =
+          err.name === "JsonWebTokenError" ? "Unauthorized" : err.message;
+        return next(createError.Unauthorized(message));
       }
       req.payload = payload;
       next();
     });
+  },
+
+  signRefreshToken: (userId) => {
+    return new Promise((resolve, reject) => {
+      const payload = {};
+      const secret = process.env.REFRESH_TOKEN_SECRET;
+      const options = {
+        expiresIn: "1y", // 1 year
+        issuer: "pickerpage.com",
+        audience: userId,
+      };
+      JWT.sign(payload, secret, options, (err, token) => {
+        if (err) {
+          console.log(err.message);
+          return reject(createError.InternalServerError());
+        }
+        resolve(token);
+      });
+    });
+  },
+
+  verifyRefreshToken: (refreshToken) => {
+    return new Promise(
+      (resolve, reject) => {
+        JWT.verify(
+          refreshToken,
+          process.env.REFRESH_TOKEN_SECRET,
+          (err, payload) => {
+            if (err) return reject(createError.Unauthorized());
+            const userId = payload.aud;
+            resolve(userId);
+          }
+        );
+      })
   },
 };
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 const createError = require("http-errors");
 const User = require("../models/user_model");
 const { authSchema } = require("../helpers/validationSchema");
-const { signAccessToken } = require("../helpers/jwtHelper");
+const {
+  signAccessToken,
+  signRefreshToken,
+  verifyRefreshToken,
+} = require("../helpers/jwtHelper");
 
 // User registration
 router.post("/register", async (req, res, next) => {
@@ -18,8 +22,10 @@ router.post("/register", async (req, res, next) => {
     const user = new User(result);
     const savedUser = await user.save();
     const accessToken = await signAccessToken(savedUser.id);
-
-    res.status(200).send({ message: "Registered Successfully.", accessToken });
+    const refreshToken = await signRefreshToken(savedUser.id);
+    res
+      .status(200)
+      .send({ message: "Registered Successfully.", accessToken, refreshToken });
   } catch (error) {
     if (error.isJoi === true) error.status = 422;
     next(error);
@@ -29,22 +35,38 @@ router.post("/register", async (req, res, next) => {
 router.post("/login", async (req, res, next) => {
   try {
     const result = await authSchema.validateAsync(req.body);
-    const user = await User.findOne({email: result.email})
-    if(!user) throw createError.NotFound("User not registered.")
+    const user = await User.findOne({ email: result.email });
+    if (!user) throw createError.NotFound("User not registered.");
 
-    const isMatched = await user.isValidPassword(result.password)
-    if(!isMatched) throw createError.Unauthorized('Username/Password not valid')
+    const isMatched = await user.isValidPassword(result.password);
+    if (!isMatched)
+      throw createError.Unauthorized("Username/Password not valid");
 
-    const accessToken = await signAccessToken(user.id)
-    res.status(200).send({message: "Successfull.." ,accessToken})
+    const accessToken = await signAccessToken(user.id);
+    const refreshToken = await signRefreshToken(user.id);
+    res
+      .status(200)
+      .send({ message: "Successfull..", accessToken, refreshToken });
   } catch (error) {
-    if(error.isJoi === true) return next(createError.BadRequest("Invalid Email/Password"))
+    if (error.isJoi === true)
+      return next(createError.BadRequest("Invalid Email/Password"));
     next(error);
   }
 });
 
 router.post("/refresh-token", async (req, res, next) => {
-  res.send({ message: "refresh-token" });
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) throw createError.BadRequest();
+    const userId = await verifyRefreshToken(refreshToken);
+
+    const newAccessToken = await signAccessToken(userId);
+    const newRefreshToken = await signRefreshToken(userId);
+    console.log(newRefreshToken);
+    res.send({ accessToken: newAccessToken, refreshToken: newRefreshToken });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete("/logout", async (req, res, next) => {
